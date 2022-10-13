a2b6dbbad19ca4098d39ecd171e1bd749aff27a4
Akshay26953 <amkharat007@gmail.com>
2022-10-13 20:32:02 +0530

    first commit

---
+++ sanitybackend/schemas/product.js
@@ -0,0 +1,44 @@
+export default {
+    name: "product",
+    title: "Product",
+    type: "document",
+    fields: [
+        {
+            name: "image",
+            title: 'Image',
+            type: 'array',
+            of: [{type: 'image'}],
+            options: {
+                hotspot: true
+            }
+        },
+        {
+            name: "name",
+            title: "Name",
+            type: "string"
+        },
+        {
+            title: 'Slug',
+            name: 'slug',
+            type: 'slug',
+            options: {
+              source: 'title',
+              maxLength: 100, 
+              slugify: input => input
+                                   .toLowerCase()
+                                   .replace(/\s+/g, '-')
+                                   .slice(0, 100)
+            }
+          },
+          {
+            name: 'price',
+            title: 'Price',
+            type: 'number'
+          },
+          {
+            name: 'details',
+            title: 'Details',
+            type: 'string'
+          }
+    ]
+}

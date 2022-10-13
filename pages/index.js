d46d38eb613abcfca1cff9aa29b456cc18cb9fe2
Akshay26953 <amkharat007@gmail.com>
2022-10-14 00:51:27 +0530

    updated index.js

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,25 @@
-import React from 'react'
-import HeroBanner from '../components/HeroBanner'
-import { client } from '../lib/client';
+import React from "react";
+import HeroBanner from "../components/HeroBanner";
+import { client } from "../lib/client";
 
-function Home() {
+function Home({ products, bannerData }) {
+  console.log(products)
   return (
     <>
-    <HeroBanner/>
+      <HeroBanner heroBanner={bannerData} />
     </>
-  )
+  );
 }
 
-export default Home
+export async function getServerSideProps() {
+  const query = `*[_type == "product"]`;
+  const products = await client.fetch(query);
+
+  const bannerQuery = `*[_type == "banner"]`;
+  const bannerData = await client.fetch(bannerQuery);
+  return {
+    props: { products, bannerData }, // will be passed to the page component as props
+  };
+}
+
+export default Home;

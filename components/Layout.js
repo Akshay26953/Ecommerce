764bf660b75868767ccb747b48de180c176f5abd
Akshay26953 <amkharat007@gmail.com>
2022-10-14 14:16:06 +0530

    added products, footerbanner

--- components/Layout.js
+++ components/Layout.js
@@ -1,8 +1,24 @@
+import Head from 'next/head'
 import React from 'react'
+import Footer from './Footer'
+import Navbar from './Navbar'
 
-function Layout() {
+function Layout({children}) {
   return (
-    <div>Layout</div>
+    <div className='layout'>
+      <Head>
+        <title>AK Store</title>
+      </Head>
+      <header>
+        <Navbar/>
+      </header>
+      <main className='main-container'>
+       {children}
+      </main>
+      <footer>
+        <Footer/>
+      </footer>
+    </div>
   )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,18 @@
 import React from "react";
+import FooterBanner from "../components/FooterBanner";
 import HeroBanner from "../components/HeroBanner";
+import Product from "../components/Product";
 import { client } from "../lib/client";
 
 function Home({ products, bannerData }) {
-  console.log(products)
+  console.log(bannerData)
   return (
     <>
-      <HeroBanner heroBanner={bannerData} />
+      <HeroBanner heroBanner={bannerData[0]} />
+      <div className="products-container">
+      {products.map((product)=> <Product key={product._id} product={product}/>)}
+      </div>
+      <FooterBanner footerBanner={bannerData[0]} />
     </>
   );
 }
